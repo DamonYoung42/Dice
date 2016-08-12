2d1c493d2297c67306ce4754e5b803d825f64d25
DamonYoung42 <damonyoung@wi.rr.com>
2016-08-12 18:17:26 -0500

    wait for key input to begin playing.

--- Dice/Game.cs
+++ Dice/Game.cs
@@ -9,7 +9,6 @@ namespace DiceGame
     class Game
     {
         public bool gameOver;
-        public bool donePlaying;
         public int round;
         public int numOfPlayers;
         public int maxScore;
@@ -21,7 +20,6 @@ namespace DiceGame
             roster = new List<Player> { };
             dice = new List<Dice> { };
             gameOver = false;
-            donePlaying = false;
             round = 0;
             numOfPlayers = 0;
             maxScore = 0;
@@ -30,13 +28,6 @@ namespace DiceGame
 
         public void SetupGame()
         {
-            roster = new List<Player> { };
-            dice = new List<Dice> { };
-            gameOver = false;
-            round = 0;
-            numOfPlayers = 0;
-            maxScore = 0;
-
             IntroduceGame();
 
             InitializePlayers();
@@ -50,11 +41,14 @@ namespace DiceGame
 
             InitializeDice();
 
+            Console.WriteLine("Press any key to begin:");
+            Console.ReadKey();
+
         }
 
         public void IntroduceGame()
         {
-            Console.WriteLine("So you want to play this dice simulation game? You can play against your friends or the computer.");
+            Console.WriteLine("So you want to play this dice game? You can play against your friends or the computer.");
             Console.WriteLine("The rules are simple: There are six dice, one each with four, six, eight, 10, 12 and 20 sides.");
             Console.WriteLine("During a player's turn, the six dice will be rolled. Add up the total number of points. The first player to reach a defined point total wins.");
             Console.WriteLine("If a player rolls a sequence of six straight numbers (i.e. 1-2-3-4-5-6), that player automatically wins.");
